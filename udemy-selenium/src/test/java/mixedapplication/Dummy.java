a68aa04c66dec88bb769bca0398694dd6e6b80d4
SayanDeRoy <sayan.de.roy@gmail.com>
2018-06-15 02:33:11 +0530

    First Commit

---
+++ udemy-selenium/src/test/java/mixedapplication/Dummy.java
@@ -0,0 +1,13 @@
+package mixedapplication;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class Dummy {
+	
+	@Test
+	public void check()
+	{
+		System.out.println("Dummy");
+	}
+}
---
+++ udemy-selenium/src/test/java/mixedapplication/DummyTest.java
@@ -0,0 +1,12 @@
+package mixedapplication;
+
+import org.testng.annotations.Test;
+
+public class DummyTest {
+	
+	@Test
+	public void check()
+	{
+		System.out.println("DummyTest");
+	}
+}
